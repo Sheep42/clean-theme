74866b7cae0484d69f686e51551063c8398c7112
Dan Shedd <dan@dshedd.com>
2020-02-15 20:32:29 -0500

    Clean up 404

--- 404.php
+++ 404.php
@@ -10,6 +10,7 @@ get_header(); ?>
 				<header class="page-header">
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'cleantheme' ); ?></h1>
 				</header><!-- .page-header -->
+				
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'cleantheme' ); ?></p>
 
