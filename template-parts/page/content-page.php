70e71b7c9f9c0374ef9e0d92a3132dfbea5c87b4
Dan Shedd <dan@dshedd.com>
2020-02-15 20:31:46 -0500

    Clean up general page template

--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -7,17 +7,19 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<h1 class="entry-title">
+			<?php esc_html_e( get_the_title() ); ?>
+		</h1>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
 			the_content();
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'cleantheme' ),
-				'after'  => '</div>',
-			) );
+			// wp_link_pages( array(
+			// 	'before' => '<div class="page-links">' . __( 'Pages:', 'cleantheme' ),
+			// 	'after'  => '</div>',
+			// ) );
 		?>
 	</div><!-- .entry-content -->
 </article><!-- #post-## -->
