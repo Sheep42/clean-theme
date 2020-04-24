9c017dbd67a2c14f2eb6663f2271efca50339c4f
Dan Shedd <dan@dshedd.com>
2020-04-23 21:48:52 -0400

    Update index.php to match archive

--- index.php
+++ index.php
@@ -11,49 +11,41 @@
 get_header(); ?>
 
 <div class="wrap">
-	<?php if ( is_home() && ! is_front_page() ) : ?>
+
+	<?php if ( have_posts() ) : ?>
 		<header class="page-header">
-			<h1 class="page-title"><?php single_post_title(); ?></h1>
+			<h1 class="page-title"><?php esc_html_e( get_option( 'page_for_posts', true ) ); ?></h1>
 		</header>
-	<?php else : ?>
-	<header class="page-header">
-		<h2 class="page-title"><?php _e( 'Posts', 'cleantheme' ); ?></h2>
-	</header>
 	<?php endif; ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<?php
+		if ( have_posts() ) : ?>
 			<?php
-			if ( have_posts() ) :
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/' . get_post_type() . '/content', 'archive' );
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/post/content', get_post_format() );
+			endwhile;
 
-				endwhile;
+			// the_posts_pagination( array(
+			// 	'prev_text' => cleantheme_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'cleantheme' ) . '</span>',
+			// 	'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'cleantheme' ) . '</span>' . cleantheme_get_svg( array( 'icon' => 'arrow-right' ) ),
+			// 	'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'cleantheme' ) . ' </span>',
+			// ) );
 
-				the_posts_pagination( array(
-					'prev_text' => '<span class="screen-reader-text">' . __( 'Previous page', 'cleantheme' ) . '</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'cleantheme' ) . '</span>',
-					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'cleantheme' ) . ' </span>',
-				) );
+		else :
 
-			else :
+			get_template_part( 'template-parts/' . get_post_type() . '/content', 'none' );
 
-				get_template_part( 'template-parts/post/content', 'none' );
-
-			endif;
-			?>
+		endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+	
 	<?php get_sidebar(); ?>
 </div><!-- .wrap -->
 
